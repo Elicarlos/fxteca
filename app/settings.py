4c0c5824a4acced8bcc9e61f5831c2d96ca5fc98
Elicarlos <elicarlosantos_@hotmail.com>
2025-01-05 13:45:58 -0300

    add dom

--- app/settings.py
+++ app/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['fuxicoteca-d2bb0ce5d25a.herokuapp.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['https://fuxicoteca-d2bb0ce5d25a.herokuapp.com/', '.fuxicoteca-d2bb0ce5d25a.herokuapp.com/', '127.0.0.1', 'localhost']
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True 
