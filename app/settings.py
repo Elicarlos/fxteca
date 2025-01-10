5d7f01667d09a7860513d85d954bb9ec0fee4cd6
Elicarlos <elicarlosantos_@hotmail.com>
2025-01-09 22:17:26 -0300

    feito atualização

--- app/settings.py
+++ app/settings.py
@@ -84,7 +84,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
-# }
+# }]
+
+
+
+# 33333
 
 
 DATABASES = {
