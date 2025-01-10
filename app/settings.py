cb0d02a553a71b657ee1377551fc8faa15531f49
Elicarlos <elicarlosantos_@hotmail.com>
2025-01-10 02:04:38 -0300

    feito atualização

--- app/settings.py
+++ app/settings.py
@@ -80,21 +80,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'app.wsgi.application'
 
 # Database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-
-
-
-
 # DATABASES = {
-#     'default': dj_database_url.config(default=config('DATABASE_URL'))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+
+DATABASES = {
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
+}
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
