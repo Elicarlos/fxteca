15cc332eae3bc2dafa890691bd744daecee36010
Elicarlos <elicarlosantos_@hotmail.com>
2025-01-10 10:00:16 -0300

    feito atualização

--- app/settings.py
+++ app/settings.py
@@ -143,7 +143,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # URL base para servir arquivos estáticos
-STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
+STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/static/"
 
 # Se quiser/precisar servir media files via outro backend S3:
 # Ex.: app/storage_backends.py (custom class)
