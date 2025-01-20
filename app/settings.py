590a009549aaf46d01b4076d96d2f19e5819653c
Elicarlos <elicarlosantos_@hotmail.com>
2025-01-20 12:06:45 -0300

    feito atualização de fonte

--- app/settings.py
+++ app/settings.py
@@ -181,38 +181,3 @@ CKEDITOR_CONFIGS = {
         "filebrowserBrowseUrl": "/ckeditor/browse/",
     },
 }
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '{levelname} {asctime} {module} {message}',
-            'style': '{',
-        },
-        'simple': {
-            'format': '{levelname} {message}',
-            'style': '{',
-        },
-    },
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose',
-        },
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'django.log',
-            'formatter': 'verbose',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console', 'file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
